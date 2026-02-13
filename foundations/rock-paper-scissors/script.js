a99c959d36a90b6b505cef5eb09afbe0f23a59af
taimv2405 <taimv2405@gmail.com>
2026-02-13 15:54:24 +0000

    Adding a UI to Rock Paper Scissors project

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -9,52 +9,85 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return prompt("Enter your choice (rock, paper, or scissors): ");
+function playRound(humanChoice, computerChoice) {
+  let result = 0;
+
+  humanChoice = humanChoice.toLowerCase();
+
+  const resultWrapper = document.querySelector(".result-wrapper");
+  
+  const divider = document.createElement("div");
+  const humanChoiceMsg = document.createElement("div");
+  const computerChoiceMsg = document.createElement("div");
+  const resultMsg = document.createElement("div");
+
+  divider.textContent = "-----------------------------------";
+  humanChoiceMsg.textContent = `Your choice: ${humanChoice}`;
+  computerChoiceMsg.textContent = `Computer choice: ${computerChoice}`;
+  
+  if (humanChoice === computerChoice) {
+    resultMsg.textContent = "Draw!";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") || 
+    (humanChoice === "paper" && computerChoice === "rock") || 
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    resultMsg.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+    result = 1;
+  } else {
+    resultMsg.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+    result = -1;
+  }
+  
+  resultWrapper.appendChild(divider);
+  resultWrapper.appendChild(humanChoiceMsg);
+  resultWrapper.appendChild(computerChoiceMsg);
+  resultWrapper.appendChild(resultMsg);
+  
+  return result;
 }
 
 function playGame() {
   let humanScore = 0;
-  let computerScore = 0;
+  let computerScore = 0;  
+  
+  const choiceBtn = document.querySelectorAll(".choice-btn");
 
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+  choiceBtn.forEach(
+    btn => btn.addEventListener("click", () => {
+      const finalResult = document.querySelector(".final-result");
+      const resultWrapper = document.querySelector(".result-wrapper");
+      
+      if (humanScore === 5 || computerScore === 5) {
+          finalResult.textContent = "";
+          while (resultWrapper.firstChild) {
+            resultWrapper.removeChild(resultWrapper.firstChild);
+          }
+          humanScore = computerScore = 0;
+      }
+      
+      const humanSelection = btn.textContent;
+      const computerSelection = getComputerChoice();
+      const result = playRound(humanSelection, computerSelection);
+      
+      if (result === 1) {
+        humanScore++;
+      } else if (result === -1) {
+        computerScore++;
+      }
+      
+      const scoreMsg = document.querySelector(".score");
+      scoreMsg.textContent = `Score: You ${humanScore} - ${computerScore} Computer`;
 
-    console.log(`Your choice: ${humanChoice}`);
-    console.log(`Computer choice: ${computerChoice}`);
-
-    if (humanChoice === computerChoice) {
-      console.log("Draw!");
-    } else if (
-      (humanChoice === "rock" && computerChoice === "scissors") || 
-      (humanChoice === "paper" && computerChoice === "rock") || 
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
-    } else {
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-      computerScore++;
-    }
-  }
-
-  for(let i = 1; i <= 5; i++) {
-    console.log(`--- Game ${i} ---`);
-    
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-  }
-
-  console.log("--------------");
-  if (humanScore > computerScore) {
-    console.log(`You win the game! Final Score: You ${humanScore} - ${computerScore} Computer`);
-  } else if (humanScore < computerScore) {
-    console.log(`You lose the game! Final Score: You ${humanScore} - ${computerScore} Computer`);
-  } else {
-    console.log(`Draw! Final Score: You ${humanScore} - ${computerScore} Computer`);
-  }
+      if (humanScore === 5) {
+        finalResult.textContent = "You win the game!";
+        finalResult.style.color = "red";
+      } else if (computerScore === 5) {
+        finalResult.textContent = "You lose the game!";
+        finalResult.style.color = "red";
+      } 
+    })
+  )
 }
 
 playGame();
